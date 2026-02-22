3dc0ed795d5dfb475d73875b13d8f56087077b21
infophreak <32310877+L0WK3Y-IAAN@users.noreply.github.com>
2026-02-22 00:50:22 -0500

    Bump version to 1.0.1 for PyPI

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='jnitrace-ex',
-    version='1.0.0',
+    version='1.0.1',
     description='JNITrace EX: trace JNI in Android apps (Frida 16+/17+)',
     long_description=long_description,
     long_description_content_type='text/markdown',
